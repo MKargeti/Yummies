56eb8b82ae8c30d8f2ff037c43d12ac880f3d456
unknown <mayank.kargeti@sapient.com>
2018-07-28 23:00:43 +0530

    upload all code

---
+++ src/js/views/searchView.js
@@ -0,0 +1,22 @@
+import { elements } from './base';
+
+export const getInput = () => elements.searchInput.val();
+
+export const clearInput = () => {
+    elements.searchInput.val('');
+};
+
+export const clearResults = () => {
+    elements.productList.html('');
+};
+
+export const emptySearchResult= () =>{
+
+    const markup=`<div class="emptyResult">
+    <h3 class="heading-secondary">Sorry, no results found!</h3>
+    <img src="/img/error-no-search-results.png">
+    <h4>Please check the spelling or try searching for something else</h4>
+    </div>`;
+
+     elements.productList.html(markup);
+};
